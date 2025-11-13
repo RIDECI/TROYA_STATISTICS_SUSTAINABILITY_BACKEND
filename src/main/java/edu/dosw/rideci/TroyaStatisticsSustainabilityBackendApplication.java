2b0fa41f4c73c87be6574c8755d604465fa92d11
Julian <julianl116910@gmail.com>
2025-11-12 22:52:13 -0500

    feat-correccion algunas carpetas y directorio de ci-cd

--- src/main/java/edu/dosw/rideci/TroyaStatisticsSustainabilityBackendApplication.java
+++ src/main/java/edu/dosw/rideci/TroyaStatisticsSustainabilityBackendApplication.java
@@ -1,4 +1,4 @@
-package edu.dosw.rideci.TROYA_STATISTICS_SUSTAINABILITY_BACKEND;
+package edu.dosw.rideci;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
