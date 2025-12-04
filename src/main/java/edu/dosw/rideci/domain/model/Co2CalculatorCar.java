5bbc2b0558b621c6d9897f1a1a85d233678a4fc0
Sebastian Barros <sebastian.barros-b@mail.escuelaing.edu.co>
2025-12-03 20:06:48 -0500

    feat implementacion Co2 Calculator

--- src/main/java/edu/dosw/rideci/domain/model/Co2CalculatorCar.java
+++ src/main/java/edu/dosw/rideci/domain/model/Co2CalculatorCar.java
@@ -4,12 +4,14 @@ import lombok.*;
 
 @Getter
 @Setter
-//@Builder
 @NoArgsConstructor
-//@AllArgsConstructor
 public class Co2CalculatorCar implements Co2Calculator {
+
+    private static final double FACTOR_CAR_KG_PER_KM = 0.192; // 192 g/km
+
     @Override
     public double calculateCO2(double distance) {
-        return 0; // lógica se implementará en dominio/services
+        if (distance <= 0) return 0;
+        return FACTOR_CAR_KG_PER_KM * distance;
     }
 }
--- src/main/java/edu/dosw/rideci/domain/model/Co2CalculatorMotorcycle.java
+++ src/main/java/edu/dosw/rideci/domain/model/Co2CalculatorMotorcycle.java
@@ -4,14 +4,16 @@ import lombok.*;
 
 @Getter
 @Setter
-//@Builder
 @NoArgsConstructor
-//@AllArgsConstructor
 public class Co2CalculatorMotorcycle implements Co2Calculator {
+
+    private static final double FACTOR_MOTO_KG_PER_KM = 0.103; // 103 g/km
 
     @Override
     public double calculateCO2(double distance) {
-        return 0;
+        if (distance <= 0) return 0;
+        return FACTOR_MOTO_KG_PER_KM * distance;
     }
 }
+
 
